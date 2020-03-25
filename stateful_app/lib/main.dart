736d350ba9195987dafd34c4a9dfe062541684fa
Francisco Guzmán <paco.mgh@gmail.com>
2020-03-25 13:43:55 -0600

    add using stateful wodget

---
+++ stateful_app/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:statefulapp/src/button.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: MyButton(),
+    )
+  );
+}
---
+++ stateful_app/lib/src/button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget{
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton>{
+  String flutterText="";
+  int index=0;
+  List<String>collections=['Flutter','es','genial'];
+
+  void onPressButton(){
+    setState(() {
+      flutterText = collections[index];
+      index=index<2?index+1:0;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful Widget"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(flutterText,
+                style: TextStyle(fontSize: 40.0),),
+              Padding(padding: EdgeInsets.all(10.0),),
+              RaisedButton(
+                child: Text("Actualizar", style: TextStyle(color: Colors.white),),
+                color: Colors.blue,
+                onPressed: onPressButton,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

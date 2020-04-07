d071d4925da5ddfb1a3a6c51adcc1f9755de30d3
Francisco Guzmán <paco.mgh@gmail.com>
2020-04-07 15:35:44 -0500

    add navigator app

---
+++ navigator_app/lib/src/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  App({Key key}) : super(key: key);
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Primera pantalla"),
+      ),
+    );
+  }
+}

f54f29b863e4df40a8c54f421b518a1beaf98127
Francisco Guzmán <paco.mgh@gmail.com>
2020-05-11 17:20:33 -0500

    add autogenerate words & styles list tile

--- infinitelistview_app/lib/src/App.dart
+++ infinitelistview_app/lib/src/App.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:english_words/english_words.dart';
 
 class App extends StatefulWidget {
   @override
@@ -7,6 +8,15 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  final suggestions = <WordPair>[];
+
+  ListTile buildRow(WordPair pair){
+    return ListTile(
+      trailing: Icon(Icons.favorite),
+      title: Text(pair.asCamelCase),
+      //subtitle: Text('Precio: ${Random().nextInt(100)} USD'),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,11 +25,13 @@ class _AppState extends State<App> {
       ),
       body: ListView.builder(
         itemBuilder: (context, i){
-          return ListTile(
-            leading: Icon(Icons.shopping_cart),
-            title: Text('Producto $i'),
-            subtitle: Text('Precio: ${Random().nextInt(100)} USD'),
-          );
+          if(i.isOdd) return Divider();
+
+          if(i>=suggestions.length){
+            suggestions.addAll(generateWordPairs().take(10));
+          }
+
+          return buildRow(suggestions[i]);
         }
       ),
     );

12353cdbd38a792a19c941c4fc3c03f4f691e738
Francisco Guzmán <paco.mgh@gmail.com>
2020-05-10 19:12:05 -0500

    add list to listview screen

--- listview_app/lib/src/Contact.dart
+++ listview_app/lib/src/Contact.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:listviewapp/src/model/contact_model.dart';
 import 'package:listviewapp/src/screens/contact_item.dart';
 
 class Contact extends StatelessWidget {
-  const Contact({Key key}): super(key:key);
+  //const Contact({Key key}): super(key:key);
+
+  buildList(){
+    return <ContactModel>[
+      ContactModel(name: "Francisco Guzman", email: "paco.mgh@gmail.com"),
+      ContactModel(name: "Pedro Perez", email: "pedroperez.52@hotmail.com"),
+      ContactModel(name: "Javier Cortes", email: "javier08cortez@hotmail.com"),
+      ContactModel(name: "Jose Perez", email: "jose.prz.1@outlook.com"),
+      ContactModel(name: "Juan Torres", email: "torresj.uan@outlook.com"),
+      ContactModel(name: "Edgar Aguirre", email: "eaguirret@outlook.com"),
+      ContactModel(name: "Juan Francisco", email: "pancho930302@gmail.com"),
+      ContactModel(name: "Juan Ramirezn", email: "rjuan9121@gmail.com"),
+    ];
+  }
+
+  List<ContactItem> buildContacList(){
+    return buildList()
+        .map<ContactItem>((contact)=> ContactItem(contact)).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,9 +30,7 @@ class Contact extends StatelessWidget {
         title: Text("Contactos"),
       ),
       body: ListView(
-        children: <Widget>[
-          ContactItem()
-        ],
+        children: buildContacList()
       ),
     );
   }
--- listview_app/lib/src/screens/contact_item.dart
+++ listview_app/lib/src/screens/contact_item.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:listviewapp/src/model/contact_model.dart';
 
 class ContactItem extends StatelessWidget {
-  const ContactItem({Key key}): super(key:key);
+  //const ContactItem({Key key}): super(key:key);
+  final ContactModel contact;
+  ContactItem(this.contact);
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: CircleAvatar(child: Text("F"),),
-      title: Text("Francisco"),
-      subtitle: Text("paco.mgh@gmail.com"),
+      leading: CircleAvatar(child: Text(contact.name[0]),),
+      title: Text(contact.name),
+      subtitle: Text(contact.email),
     );
   }
 }

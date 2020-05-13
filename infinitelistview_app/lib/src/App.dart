d3db4293a47cfb726d5528eed07b81abc6356045
Francisco Guzmán <paco.mgh@gmail.com>
2020-05-12 22:07:41 -0500

    add pageroute with favs elements

--- infinitelistview_app/lib/src/App.dart
+++ infinitelistview_app/lib/src/App.dart
@@ -9,30 +9,53 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   final suggestions = <WordPair>[];
+  final saved = <WordPair>[];
 
-  ListTile buildRow(WordPair pair){
+  ListTile buildRow(WordPair pair) {
     return ListTile(
       trailing: Icon(Icons.favorite),
       title: Text(pair.asCamelCase),
       //subtitle: Text('Precio: ${Random().nextInt(100)} USD'),
+      onTap: () {
+        saved.add(pair);
+      },
     );
   }
+
   @override
   Widget build(BuildContext context) {
+    void pushSaved() {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) {
+            final tiles = saved.map<ListTile>((pair)=>ListTile(title: Text(pair.asPascalCase),))
+            .toList();
+            return Scaffold(
+                appBar: AppBar(
+                  title: Text("Favoritos",),
+                ),
+              body: ListView(children: tiles),
+            );
+          })
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista infinita"),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: pushSaved)
+        ],
       ),
       body: ListView.builder(
-        itemBuilder: (context, i){
-          if(i.isOdd) return Divider();
+          itemBuilder: (context, i) {
+            if (i.isOdd) return Divider();
 
-          if(i>=suggestions.length){
-            suggestions.addAll(generateWordPairs().take(10));
+            if (i >= suggestions.length) {
+              suggestions.addAll(generateWordPairs().take(10));
+            }
+
+            return buildRow(suggestions[i]);
           }
-
-          return buildRow(suggestions[i]);
-        }
       ),
     );
   }

c91c2776a87b9819d05ebafe521fa9db67a7ba52
Francisco Guzmán <paco.mgh@gmail.com>
2020-07-08 11:53:49 -0500

    add buttons and styles to main screen

--- flutter_chat_app/lib/screens/welcome_screen.dart
+++ flutter_chat_app/lib/screens/welcome_screen.dart
@@ -21,9 +21,29 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 Text("Flutter chat",
                     style: TextStyle(fontSize: 35.0, fontWeight: FontWeight.w600))
               ],
-            )
+            ),
+            SizedBox(height: 48.0,),
+            getButton(Colors.lightBlueAccent, "Log in", (){}),
+            getButton(Colors.blueAccent, "Sign in", (){})
           ],
         ),
+      ),
+    );
+  }
+
+  Widget getButton(MaterialAccentColor color, String name, VoidCallback onPress){
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        color: color,
+        borderRadius: BorderRadius.circular(30.0),
+        elevation: 5.0,
+        child: SizedBox(
+          height: 43.0,
+          child: FlatButton(
+            child: Text(name, style: TextStyle(color: Colors.white),),
+            onPressed: onPress,
+        )),
       ),
     );
   }

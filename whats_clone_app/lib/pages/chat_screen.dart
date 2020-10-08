4abacd0aaf488d16f3dae81b6d2df112dbcd3d3f
Francisco Guzmán <paco.mgh@gmail.com>
2020-10-08 18:54:12 -0500

    add styles to sended message

--- whats_clone_app/lib/pages/chat_screen.dart
+++ whats_clone_app/lib/pages/chat_screen.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:whatscloneapp/models/chat_model.dart';
 
 class ChatScreen extends StatefulWidget {
   final String name;
-
   ChatScreen({this.name});
 
   @override
@@ -10,6 +11,8 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
+  //variable para controlar el envio de mensajes cuando nos e escribe algo
+  bool _isTyped = false;
   //sirve como un notificador para saber cuandop cambio el texto
   final TextEditingController _textController = new TextEditingController();
   //variable que almacena los mensajes que recibimos
@@ -27,6 +30,10 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
             //mandamos la lista de mensajes
             new Flexible(
               child: new ListView.builder(
+                padding: new EdgeInsets.all(8),
+                //seteando reverse esta propiedad en true, el widget muestra la informacion
+                //desde abajo hacia arriba
+                reverse: true,
                 //desde donde la obtenemos
                 itemBuilder: (_, int index) => _messages[index],
                 //item counr nos devuelve la cantidad de mensajes que le indiquemos
@@ -47,6 +54,12 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   }
 
   void _handledSubmit(String text){
+    //limpiamos el contenido del texto cuando se dispara el evento, es decir picamos enviar
+    _textController.clear();
+    //reseteamos la bandera isTyped para que el valor sea falso y no se envie texto
+    setState(() {
+      _isTyped = false;
+    });
     //recibimos el mensaje que se envia del widget principal y enviarlo a este widget
     //despues de que lo tome lo agregamos a la lista a traves de setState
     //setState nos permitira redibujar el widget para poder tomar la coleccion desde otros lugares
@@ -65,6 +78,13 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
     //insertamos el widget que creamos, esto en el settate
     setState(() {
       _messages.insert(0, message);
+      //hacemos que se muestre el ultimo mensaje
+      //guardamos en una variable el contenido que se obtiene del ultimo mensaje
+      //retornamos el contenido que es igual al contenido de este nombre
+      var data = messageData.firstWhere((t) => t.name == widget.name);
+    //actualizamos el mensje que se muestra, va a ser igual al mensaje que ingresamos
+      //en el texto
+      data.message = message.text;
     });
     //iondicamos que el animation controller empiece a correr la animacion
     message.animationController.forward();
@@ -85,12 +105,22 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
               new Flexible(
                 child: new TextField(
                   controller: _textController,
+                  onChanged: (String text){
+                    setState(() {
+                      //si contiene algo regresa un true, sino un false
+                      _isTyped = text.length > 0;
+                    });
+                  },
+                  //colocamos un placeholder para envuiar un mensaje
+                  decoration: new InputDecoration.collapsed(hintText: "Enviar mensaje"),
                 ),
               ),
               new Container(
                 child: new IconButton(
                     icon: new Icon(Icons.send),
-                    onPressed: () => _handledSubmit(_textController.text)
+                    //si istyped es true habilita el boton y envia mensaje
+                    onPressed: _isTyped ?
+                        () => _handledSubmit(_textController.text) : null,
                   ),
               ),
             ],
@@ -120,10 +150,13 @@ class ChatMessage extends StatelessWidget {
           curve: Curves.easeOut
       ),
       child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
         child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           //definimos los widget que tendra
           children: <Widget>[
             new Container(
+              margin: const EdgeInsets.only(right: 16),
               //definimos el icono redondo que tendra
               child: new CircleAvatar(
                 //recibe el nombre, solo la letra
@@ -133,10 +166,13 @@ class ChatMessage extends StatelessWidget {
             ////este widget se expande para ocupar el espacio habilitado
             new Expanded(
               child: new Column(
+                //alineamos el contenido(texto hacia el iniciod el widget
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   //llamamos al texto del nombre del contacto que lo envia
-                  new Text(name),
+                  new Text(name, style: Theme.of(context).textTheme.subtitle1,),
                   new Container(
+                    margin: const EdgeInsets.only(top: 5),
                     //colocamos el mensaje
                     child: new Text(text),
                   )
